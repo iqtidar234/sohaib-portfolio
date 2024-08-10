8f1bcc3d131b8ddc04139e25be6ee1443d4e7222
iqtidar234 <iqtidar2345@gmail.com>
2024-08-10 16:48:26 +0500

    image changed

--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -22,12 +22,12 @@ const Photo = () => {
           className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] mix-blend-lighten absolute "
         >
           <Image
-            src="/assets/photo.png"
+            src="/assets/hasnain.jpg"
             priority
             quality={100}
             fill
             alt=""
-            className="object-contain"
+            className="object-contain rounded-full"
           />
         </motion.div>
 
